0e4f60043db8a4f02e5d6b86ba051106cbdaf204
Vulich Fernando <46904390+fvulich@users.noreply.github.com>
2021-03-26 16:20:56 -0300

    Make docChanges() return an empty array

--- mocks/helpers/buildQuerySnapShot.js
+++ mocks/helpers/buildQuerySnapShot.js
@@ -6,24 +6,18 @@ const buildDocFromHash = require('./buildDocFromHash');
  * @param {*[]} requestedRecords
  */
 module.exports = function buildQuerySnapShot(requestedRecords) {
-  const multipleRecords = requestedRecords.filter(rec => !!rec);
-  const docs = multipleRecords.map(buildDocFromHash);
+	const multipleRecords = requestedRecords.filter(rec => !!rec);
+	const docs = multipleRecords.map(buildDocFromHash);
 
-  return {
-    empty: multipleRecords.length < 1,
-    size: multipleRecords.length,
-    docs,
-    forEach(callback) {
-      docs.forEach(callback);
-    },
-    docChanges() {
-      return {
-        forEach(callback) {
-          // eslint-disable-next-line no-console
-          console.info('Firestore jest mock does not currently support tracking changes');
-          callback();
-        },
-      };
-    },
-  };
+	return {
+		empty: multipleRecords.length < 1,
+		size: multipleRecords.length,
+		docs,
+		forEach(callback) {
+			docs.forEach(callback);
+		},
+		docChanges() {
+			return [];
+		},
+	};
 };
--- prettier.config.js
+++ prettier.config.js
@@ -1,1 +1,12 @@
-module.exports = require('@upstatement/prettier-config');
+module.exports = {
+	"arrowParens": "avoid",
+	"singleQuote": true,
+	"semi": true,
+	"trailingComma": "es5",
+	"useTabs": true,
+	"tabWidth": 4,
+	"bracketSpacing": true,
+	"proseWrap": "never",
+	"printWidth": 150,
+	"endOfLine": "auto"
+}
